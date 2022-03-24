3b6f731a7aa42e575979fe37fd9867e9b899c521
Damian Tobiasz <damiant12345@gmail.com>
2022-03-24 18:20:08 +0100

    Commit

---
+++ src/app/pages/home.page.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'app-home-page', templateUrl: './home.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class HomePage {
+}
